29cc109f79296d152b1f6eb4a09b69d5b3683460
SnowCoder404 <contact@marco-lenschau.de>
2025-04-09 02:56:43 +0200

    Component: Outsourcing logo in a own component. Style: Finish the footer.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { LogoComponent } from '../logo/logo.component';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [LogoComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
