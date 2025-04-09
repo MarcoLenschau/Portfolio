40b8f5cb013fab1685b58b48519877478b320a76
SnowCoder404 <contact@marco-lenschau.de>
2025-04-09 02:01:42 +0200

    Component: Add contact-me component.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,9 +3,10 @@ import { WhyMeComponent } from '../why-me/why-me.component';
 import { SkillsComponent } from '../skills/skills.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { FeedbackSectionComponent } from '../feedback-section/feedback-section.component';
+import { ContactMeComponent } from '../contact-me/contact-me.component';
 @Component({
   selector: 'app-main',
-  imports: [WhyMeComponent, SkillsComponent, ProjectsComponent, FeedbackSectionComponent],
+  imports: [WhyMeComponent, SkillsComponent, ProjectsComponent, FeedbackSectionComponent, ContactMeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
